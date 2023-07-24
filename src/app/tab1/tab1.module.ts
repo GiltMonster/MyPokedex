309c2669e84d559da775bd533131055cdc1912d3
GiltMonster <lucassantoslukas2@gmail.com>
2023-07-23 23:10:34 -0300

    I prepared tab1, to receive the list

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -6,6 +6,7 @@ import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
+import { ListPokemonModule } from '../view/list-pokemon/list-pokemon.module';
 
 @NgModule({
   imports: [
@@ -13,7 +14,8 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab1PageRoutingModule
+    Tab1PageRoutingModule,
+    ListPokemonModule
   ],
   declarations: [Tab1Page]
 })
